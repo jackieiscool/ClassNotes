3a4ec3855ed5f79bc1ce1a8083528ffda6c74ca5
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-06-26 11:39:59 -0700

    sdjhgsd

--- wk2d2/sinatra-demo/calc.rb
+++ wk2d2/sinatra-demo/calc.rb
@@ -1,23 +1,23 @@
-# require 'sinatra'
-# require 'sinatra/reloader'
+require 'sinatra'
+require 'sinatra/reloader'
 
 
-# get '/multiply/:num1/:num2' do
-# 	answer = params[:num1] * params[:num2]
-# 	'#{answer}'
-# end
+get '/multiply/:num1/:num2' do
+	answer = params[:num1] * params[:num2]
+	'#{answer}'
+end
 
-# get '/divide/:num1/:num2' do
-# 	answer = params[:num1] / params[:num2]
-# 	'#{answer}'
-# end
+get '/divide/:num1/:num2' do
+	answer = params[:num1] / params[:num2]
+	'#{answer}'
+end
 
-# get '/add/:num1/:num2' do
-# 	answer = params[:num1] + params[:num2]
-# 	'#{answer}'
-# end
+get '/add/:num1/:num2' do
+	answer = params[:num1] + params[:num2]
+	'#{answer}'
+end
 
-# get '/subtract/:num1/:num2' do
-# 	answer = params[:num1] - params[:num2]
-# 	'#{answer}'
-# end
+get '/subtract/:num1/:num2' do
+	answer = params[:num1] - params[:num2]
+	'#{answer}'
+end
