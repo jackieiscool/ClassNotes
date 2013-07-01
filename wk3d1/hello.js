22fd9888ef3adc82b9a0f5d9db2e0f3134976de3
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-07-01 09:25:11 -0700

    Javascript

---
+++ wk3d1/hello.js
@@ -0,0 +1,18 @@
+// This is the main function for my app
+function main() {
+	"use strict";
+
+	console.log("hello world");
+
+	var i = 4,
+	str = "hello world",
+	arr = [2, 4, 19, 3],
+	obj = { awesome: "Megan", wayAwesome: "Brian"};
+
+	console.log("number", i);
+	console.log("string", str);
+	console.log("array", arr);
+	console.log('object', obj);
+}
+
+main();
---
+++ wk3d1/people_picker.md
@@ -0,0 +1,6 @@
+* Create an array with everyone in teh class  
+* Accept arguments for `groupSize` or `numberOfPeople`  
+* Prints out rows of groups  
+* Or prints out array of people  
+
+Bonus: use jQuery to take arguments from `input` elements (and a run button)  
